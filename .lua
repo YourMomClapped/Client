357aeae84b735516cbd651b894aac056576b2e8f
YourMomClapped <91243291+YourMomClapped@users.noreply.github.com>
2022-07-17 03:02:16 +0930

    Update .lua

--- .lua
+++ .lua
@@ -7,6 +7,7 @@ if Player.UserId == 1201621667 then
 
 
 -- MY ALTS ACCESS
+wait(1)
 if Player.UserId == 3444568744 then
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/YourMomClapped/Annon-Hub/main/.lua'),true))()
 end
